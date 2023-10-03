370eaa7350bddf1d0a0a7d208915dd71bb119357
Jean Deruelle <jean.deruelle@jikigai.com>
2023-10-03 13:13:50 +0200

    Adding Performance Tests

--- jain-sip-performance/src/main/resources/tuneOS.sh
+++ jain-sip-performance/src/main/resources/tuneOS.sh
@@ -3,6 +3,7 @@
 echo "=== Tuning OS ==="
 ##################################
 set +e
-! sudo sh -c "cat ./sysctl.conf > /etc/sysctl.conf"
+! sudo sh -c "cat $WORKSPACE/jain-sip-performance/src/main/resources/sysctl.conf > /etc/sysctl.conf"
 ! sudo sysctl -p
-! sudo sh -c "ulimit -n 65535 && exec su jenkins"
+## ! sudo sh -c "ulimit -n 65535 && exec su jenkins"
+! sudo sh -c "ulimit -n 65535"
