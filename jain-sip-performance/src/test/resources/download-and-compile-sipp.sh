49ef1d4b84cc88a8d7549005542659e35d578e17
Jean Deruelle <jean.deruelle@jikigai.com>
2023-10-10 10:23:16 +0200

    MS-6 fix: updating sipp script

--- jain-sip-performance/src/test/resources/download-and-compile-sipp.sh
+++ jain-sip-performance/src/test/resources/download-and-compile-sipp.sh
@@ -1,4 +1,4 @@
 # Download sipp
 wget -m -nd https://github.com/SIPp/sipp/releases/download/v3.7.1/sipp
-mv -f sipp $WORKSPACE/jain-sip-performance/src/main/resources/sipp
-chmod 777 $WORKSPACE/jain-sip-performance/src/main/resources/sipp
+mv -f sipp $WORKSPACE/jain-sip-performance/src/test/resources/sipp
+chmod 777 $WORKSPACE/jain-sip-performance/src/test/resources/sipp
