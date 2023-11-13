4d2a6b0b19fc68927cabd78f2014d8e3ac047181
Jean Deruelle <jean.deruelle@jikigai.com>
2023-11-13 09:52:16 +0100

    MS-28 feat: Added License to all new Netty files

--- jain-sip-ri/src/main/java/gov/nist/javax/sip/parser/NettyMessageParser.java
+++ jain-sip-ri/src/main/java/gov/nist/javax/sip/parser/NettyMessageParser.java
@@ -1,4 +1,22 @@
 
+/*
+ * Mobius Software LTD
+ * Copyright 2023, Mobius Software LTD and individual contributors
+ * by the @authors tag.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation; either version 3 of
+ * the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>
+ */
 package gov.nist.javax.sip.parser;
 
 import java.io.IOException;
@@ -14,6 +32,11 @@ import gov.nist.javax.sip.message.SIPMessage;
 import gov.nist.javax.sip.message.SIPRequest;
 import io.netty.buffer.ByteBuf;
 
+/**
+ * SIP Message Parser based on Netty ByteBuf
+ *  
+ * @author Jean Deruelle
+ */
 public class NettyMessageParser {
     private static StackLogger logger = CommonLogger.getLogger(NettyMessageParser.class);
 
