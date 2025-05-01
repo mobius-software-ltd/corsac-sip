61ab7bf933ac8894544382ccc83bccabce372e98
Yulian Oifa <yulian.oifa@mobius-software.com>
2025-05-01 15:10:17 +0200

    Reverting the Dialog Linger

--- sip-ri/src/main/java/gov/nist/javax/sip/stack/DialogLingerTimer.java
+++ sip-ri/src/main/java/gov/nist/javax/sip/stack/DialogLingerTimer.java
@@ -11,35 +11,32 @@ public class DialogLingerTimer extends SIPStackTimerTask implements Serializable
 	
 	private SIPTransactionStack sipStack;
 	private SipProviderImpl sipProvider;
-	private String dialogId;
+	private SIPDialog dialog;
 	private String callId;
 	
-	DialogLingerTimer(SIPTransactionStack sipStack, SipProviderImpl sipProvider, String dialogId,String callId){
+	DialogLingerTimer(SIPTransactionStack sipStack, SipProviderImpl sipProvider, SIPDialog dialog,String callId){
 		super(DialogLingerTimer.class.getSimpleName());
 		this.sipStack = sipStack;
 		this.sipProvider = sipProvider;
-		this.dialogId = dialogId;
+		this.dialog = dialog;
 		this.callId = callId;
 	}
 
     public void runTask() {
-    	SIPDialog dialog = sipStack.getDialog(dialogId);
-    	if(dialog!=null) {
-	    	try {
-				sipStack.removeDialog(dialog, sipProvider);
-			}
-			catch (Exception e) {
-				e.printStackTrace();
-			}
-			// Issue 279 :
-	        // https://jain-sip.dev.java.net/issues/show_bug.cgi?id=279
-	        // if non reentrant listener is used the event delivery of
-	        // DialogTerminated
-	        // can happen after the clean
-	        if (((SipStackImpl) sipStack).isReEntrantListener()) {
-	             ((SIPDialog)dialog).cleanUp();
-	        }
-    	}
+    	try {
+			sipStack.removeDialog(dialog, sipProvider);
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+		}
+		// Issue 279 :
+        // https://jain-sip.dev.java.net/issues/show_bug.cgi?id=279
+        // if non reentrant listener is used the event delivery of
+        // DialogTerminated
+        // can happen after the clean
+        if (((SipStackImpl) sipStack).isReEntrantListener()) {
+             ((SIPDialog)dialog).cleanUp();
+        }
     }
 
     @Override
